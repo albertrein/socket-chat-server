ce0be8df3f8e6fa66c0abb0e1d9eaeead025fb30
albertrein <guilhermealberto8@gmail.com>
2023-01-23 09:55:52 -0300

    Adicionando server na raiz e habilitando arquivos estaticos.

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const path = require('path');
+
+app.use('/assets', express.static('assets'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +'/index.html')
+})
+
+app.get('/cript', (req, res) => {
+    res.sendFile(__dirname +'/cript-teste.html')
+})
+
+const server = require('http').createServer(app);
+
+
+const io = require('socket.io')(server, {
+    cors: { origin: "*"}
+});
+
+
+io.on('connection', (socket) => {
+    console.log('connection');
+ 
+    socket.on('sendChatToServer', (message) => {
+        console.log(message);
+
+        // io.sockets.emit('sendChatToClient', message);
+        socket.broadcast.emit('sendChatToClient', message);
+    });
+    
+    socket.on("sendFile", (file) => {
+        console.log(file);
+        //console.log(file.toString('base64'));
+        socket.broadcast.emit('receivedFiles', {"arquivo" : file.arquivo.toString('base64'), "extensao" : file.extensao});
+    });
+
+
+    socket.on('disconnect', (socket) => {
+        console.log('Disconnect');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running');
+});
