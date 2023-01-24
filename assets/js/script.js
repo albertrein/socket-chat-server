86be7d1d9c11286409ceefb23c0ecbf4062803b5
Pedro Vargas <pedrofreitasvgas@gmail.com>
2023-01-24 08:29:48 -0300

    feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let mensagens = [];
 
 $(function () {
 
-  let ip_address = '127.0.0.1';
+  let ip_address = '192.168.1.36';
   let socket_port = '3000';
   let socket = io(ip_address + ':' + socket_port);
   let chatInput = $('#chatInput');
